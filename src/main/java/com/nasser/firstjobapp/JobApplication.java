1c3c5a2a8d210107e5107aa3c7d25cb07ccea610
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-05 01:50:53 +0300

    initial commit

---
+++ src/main/java/com/nasser/firstjobapp/JobApplication.java
@@ -0,0 +1,13 @@
+package com.nasser.firstjobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+
+}
