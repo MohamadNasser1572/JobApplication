f736d6906524ec7df1ea98d107f9cab18b1aad5d
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-05 10:59:10 +0300

    add getters and setters in Job class+ add createJob method in job controller + create job service class

--- src/main/java/com/nasser/firstjobapp/job/Job.java
+++ src/main/java/com/nasser/firstjobapp/job/Job.java
@@ -18,4 +18,52 @@ public class Job {
         this.maxSalary = maxSalary;
         this.location = location;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMidSalary() {
+        return midSalary;
+    }
+
+    public void setMidSalary(String midSalary) {
+        this.midSalary = midSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/com/nasser/firstjobapp/job/JobController.java
+++ src/main/java/com/nasser/firstjobapp/job/JobController.java
@@ -1,10 +1,13 @@
 package com.nasser.firstjobapp.job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 public class JobController {
     private List<Job> jobs = new ArrayList<Job>();
@@ -13,4 +16,32 @@ public class JobController {
     public List<Job> findAll() {
         return jobs;
     }
+
+
+    //POST /jobs: create a new job(request body should contain job details)
+    @PostMapping("/jobs")
+    public String createJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "Job created successfully.";
+    }
 }
+
+/*
+ * GET /jobs: Get all jobs
+ * GET /jobs/{id}: Get a specific job by ID
+ * POST /jobs: create a new job(request body should contain job details)
+ * DELETE /jobs/{id} delete a specific job by id
+ * PUT /jobs/{id}: update a specific job by ID (request body should contain the updated job details)
+ * GET /jobs/{id}/company: Get the company associated with a specific job by ID
+
+ * Example API URLs:
+ * GET {base_url}/jobs
+ * GET {base_url}/jobs/1
+ * POST {base_url}/jobs
+ * DELETE {base_url}/jobs/1
+ * PUT {base_url}/jobs/1
+ *
+
+
+
+ */
